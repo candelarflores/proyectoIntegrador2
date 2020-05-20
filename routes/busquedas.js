817b1acf00f804be6dd5f4fb47ddce175a36975b
Cande <cflores@udesa.edu.ar>
2020-05-20 19:44:20 -0300

    Avance buscador de usuarios

--- routes/busquedas.js
+++ routes/busquedas.js
@@ -8,7 +8,13 @@ router.get('/', controller.busqueda);
 /* localhost:3000/buscar/resultados */
 router.get('/resultados', controller.resultados);
 
-/* localhost:3000/buscar/resultados_usuarios */
-router.get('/resultados_usuarios', controller.resultadosUsuarios);
+/* localhost:3000/buscar/busqueda_usuarios */
+router.get('/busqueda_usuarios', controller.busquedaUsuarios);
+
+/* localhost:3000/buscar/busqueda_usuarios/resultadosUsuarios:id */
+router.get('/busqueda_usuarios/resultadosUsuarios:id', controller.resultadosUsuarios);
+
+/* localhost:3000/buscar/busqueda_usuarios/resultadosUsuarios:id/detalle */
+router.get('/busqueda_usuarios/resultadosUsuarios:id/detalle', controller.detalleUsuarios);
 
 module.exports = router;
