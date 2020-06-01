9ea9c2ebc997882483b37960f32733a119280ecd
Joaquin <joaco.rebelo@gmail.com>
2020-06-01 18:55:14 -0300

    Form de detalle y estilo de estrella

--- controllers/detallesController.js
+++ controllers/detallesController.js
@@ -1,8 +1,29 @@
+let db = require("../database/models");
+const moduloLogin = require("../modulo-login");
 module.exports = {
 
  detalles: function(req, res){
 
-    res.render('detalle')
- }
-
+   res.render('detalle', {
+      serieId: req.query.serieId
+   })
+},
+create: function(req, res) {
+  moduloLogin.validar(req.body.email, req.body.pwd)
+  .then(function(usuario) {
+     if(usuario != undefined) {
+        db.Reseñas.create({
+           movie_id: req.body.serieId,
+           user_id: usuario.id,
+           content: req.body.text,
+           rating: req.body.star
+        })
+        .then(function(resultado) {
+           res.redirect("/detalle")
+        })
+     } else {
+        res.redirect("/registro")
+     }
+  })
+},
 }
--- routes/detalles.js
+++ routes/detalles.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/detallesController');
 
 /* localhost:3000/detalles */
 router.get('/', controller.detalles);
+router.post('/', controller.create);
 
 module.exports = router;
