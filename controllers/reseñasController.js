accfd783b6b88148de776d239ca27b9ec068f1d4
Ryosho <eshimizukawa@udesa.edu.ar>
2020-06-04 20:07:19 -0300

    Mis reseñas

--- controllers/reseñasController.js
+++ controllers/reseñasController.js
@@ -1,5 +1,6 @@
 const DB = require('../database/models');
 const OP = DB.Sequelize.Op;
+const moduloLogin = require('../modulo-login');
 
 module.exports = {
 
@@ -15,8 +16,78 @@ module.exports = {
             // res.send(reseñas)
           })
    
-       
-    }
+       //<%=resultado.content%>
+    },
+
+     edicion: function(req, res){ 
+      res.render('reseñasEdit', {id: req.params.id})
+  
+      },
+    
+
+    editar: function(req, res){
+      moduloLogin.validar(req.body.email, req.body.pswd)
+         .then(function(resultado) {
+             if(resultado != undefined){
+              DB.Reseñas.update({
+                content: req.body.text,
+                rating: req.body.star,
+        
+              }, {
+                where: {
+                  id: req.body.serieId,
+                }
+              })
+              .then(function(){
+                res.redirect('/resenias/' + resultado.id);
+              })
+
+             } else {
+               return res.redirect('/resenias/edit/' + req.params.id);
+             }
+           
+         });
+      
+    },
+    
+    delete: function(req, res) {
+      res.render('reseñasDelete', {id:req.params.id})
+      
+    },
+
+    borrar: function(req, res){
+       moduloLogin.validar(req.body.email, req.body.pswd)
+        .then(function(resultado){
+          if(resultado != null){
+            DB.Reseñas.destroy({
+              where: {
+                id: req.body.idResenia
+              }
+              
+            })
+            res.redirect('/resenias/' + resultado.id);
+          } else{
+            res.redirect('/resenias/delete/' + req.params.id)
+          }
+        })
+
+    },
+
+    editarr: function(req, res){
+      DB.Reseñas.findOne({where: [
+        {id: req.params.id}
+      ]
+
+      })
+      .then(function(resultado){
+        res.render('reseñasEdit', {resultado: resultado })
+      })
+    },
+
+  
+  
+    
     
    
+
    }
--- routes/reseñas.js
+++ routes/reseñas.js
@@ -5,6 +5,14 @@ const controller = require('../controllers/reseñasController')
 
 router.get('/:id', controller.reseña)
 /* GET home page. */
+router.get('/edit/:id', controller.edicion)
+
+router.post('/editE', controller.editar)
+
+
+router.get('/delete/:id', controller.delete)
+
+router.post('/deleteR', controller.borrar)
 
 
 module.exports = router;
